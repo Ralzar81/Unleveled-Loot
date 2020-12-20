da390610ffe5323aa684a5102afd159c6e860775
Ralzar81 <57372363+Ralzar81@users.noreply.github.com>
2020-12-20 23:44:01 +0100

    Add files via upload

--- Unleveled Loot/UnleveledLoot.cs
+++ Unleveled Loot/UnleveledLoot.cs
@@ -8,7 +8,7 @@ using DaggerfallWorkshop;
 using UnityEngine;
 using System;
 using System.Collections.Generic;
-
+using DaggerfallWorkshop.Game.Utility;
 
 namespace UnleveledLootMod
 {
@@ -17,6 +17,8 @@ namespace UnleveledLootMod
         static Mod mod;
         static int luckMod;
         static int matRoll;
+        static int region = 0;
+        static int dungeon = 0;
 
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
@@ -25,33 +27,45 @@ namespace UnleveledLootMod
             var go = new GameObject(mod.Title);
             go.AddComponent<UnleveledLoot>();
         }
-
+        
         void Awake()
         {
-            //Unleveled gold.
+            //Unleveled gold and daedra drops
             FormulaHelper.RegisterOverride(mod, "ModifyFoundLootItems", (Func<DaggerfallUnityItem[], int>)UnleveledGoldLootPiles);
             EnemyDeath.OnEnemyDeath += UnlevelDroppedLoot_OnEnemyDeath;
+
+            PlayerEnterExit.OnPreTransition += SetDungeon_OnPreTransition;
+            PlayerEnterExit.OnTransitionExterior += ClearData_OnTransitionExterior;
 
             //Override of material mechanics.
             FormulaHelper.RegisterOverride<Func<int, WeaponMaterialTypes>>(mod, "RandomMaterial", (player) =>
             {
                 luckMod = GameManager.Instance.PlayerEntity.Stats.LiveLuck / 10;
-                matRoll = UnityEngine.Random.Range(0, 91) + luckMod;
+                matRoll = UnityEngine.Random.Range(0, 92) + luckMod;
                 WeaponMaterialTypes material = WpnMatSelector();
-                Debug.Log("[Unleveled Loot] matRoll = " + matRoll.ToString());
                 return material;
             });
             FormulaHelper.RegisterOverride<Func<int, ArmorMaterialTypes>>(mod, "RandomArmorMaterial", (player) =>
             {
                 luckMod = GameManager.Instance.PlayerEntity.Stats.LiveLuck / 10;
-                matRoll = UnityEngine.Random.Range(0, 91) + luckMod;
+                matRoll = UnityEngine.Random.Range(0, 92) + luckMod;
                 ArmorMaterialTypes material = ArmMatSelector();
-                Debug.Log("[Unleveled Loot] matRoll = " + matRoll.ToString());
                 return material;
             });
 
 
             mod.IsReady = true;
+        }
+
+        private static void SetDungeon_OnPreTransition(PlayerEnterExit.TransitionEventArgs args)
+        {
+            region = GameManager.Instance.PlayerGPS.CurrentRegionIndex;
+            dungeon = (int)GameManager.Instance.PlayerGPS.CurrentLocation.MapTableData.DungeonType;
+        }
+
+        private static void ClearData_OnTransitionExterior(PlayerEnterExit.TransitionEventArgs args)
+        {
+            dungeon = -1;
         }
 
         //Code for possibly unleveled gold.
@@ -81,7 +95,7 @@ namespace UnleveledLootMod
         }
 
         public static void UnlevelDroppedLoot_OnEnemyDeath(object sender, EventArgs e)
-        {            
+        {
             EnemyDeath enemyDeath = sender as EnemyDeath;
             if (enemyDeath != null)
             {
@@ -91,12 +105,15 @@ namespace UnleveledLootMod
                     EnemyEntity enemyEntity = entityBehaviour.Entity as EnemyEntity;
                     if (enemyEntity != null)
                     {
-                        Debug.Log(enemyEntity.Name);
                         luckMod = GameManager.Instance.PlayerEntity.Stats.LiveLuck / 10;
 
                         if (enemyEntity.MobileEnemy.Affinity == MobileAffinity.Daedra)
                         {
                             AddDaedric(entityBehaviour.CorpseLootContainer.Items, enemyEntity.MobileEnemy.ID);
+                        }
+                        else if (enemyEntity.MobileEnemy.Team == MobileTeams.Orcs)
+                        {
+                            AddOrcish(entityBehaviour.CorpseLootContainer.Items, enemyEntity.MobileEnemy.ID);
                         }
 
                         //Code for possibly unleveled gold.
@@ -106,6 +123,7 @@ namespace UnleveledLootMod
                             gold.stackCount /= GameManager.Instance.PlayerEntity.Level;
                             if (gold.stackCount < 1)
                                 gold.stackCount = 1;
+                            gold.stackCount *= UnityEngine.Random.Range(1, luckMod + 1);
                         }
                     }
                 }
@@ -118,13 +136,16 @@ namespace UnleveledLootMod
         private static ArmorMaterialTypes ArmMatSelector()
         {
             ArmorMaterialTypes armorMaterial = ArmorMaterialTypes.Leather;
-            int shopMod = 0;
-            if (GameManager.Instance.PlayerEnterExit.IsPlayerInsideOpenShop)
-                shopMod += GameManager.Instance.PlayerEnterExit.Interior.BuildingData.Quality;
+            int rollMod = 0;
+            int armRoll = UnityEngine.Random.Range(1, 91) + luckMod;
 
-            if (matRoll + shopMod > 60)
+            if (GameManager.Instance.PlayerEnterExit.IsPlayerInsideOpenShop)
+                rollMod += GameManager.Instance.PlayerEnterExit.Interior.BuildingData.Quality * 6;
+            else if (GameManager.Instance.PlayerEnterExit.IsPlayerInsideDungeon)
+                rollMod += dungeonQuality();
+            if (armRoll + rollMod > 50)
             {
-                if (matRoll + shopMod > 90)
+                if (armRoll + rollMod > 80)
                 {
                     WeaponMaterialTypes material = WpnMatSelector();
                     armorMaterial = (ArmorMaterialTypes)(0x0200 + material);
@@ -152,52 +173,88 @@ namespace UnleveledLootMod
         private static WeaponMaterialTypes randomMat()
         {
             WeaponMaterialTypes weaponMaterial = WeaponMaterialTypes.Iron;
-            int region = GameManager.Instance.PlayerGPS.CurrentRegionIndex;
-            int dungeon = GameManager.Instance.PlayerEnterExit.Dungeon.Summary.ID;
-            int roll = UnityEngine.Random.Range(0, 90) + luckMod;
-
+            if (GameManager.Instance.PlayerEnterExit.IsPlayerInsideDungeon)
+                matRoll = (matRoll - 15) + dungeonQuality();
+            else
+                matRoll -= UnityEngine.Random.Range(10, 20);
             //increased chance of orcish when in orsinium or an orc stronghold
-            if ((region == (int)DaggerfallRegions.OrsiniumArea || dungeon == (int)DFRegion.DungeonTypes.OrcStronghold) && matRoll > 70)
+            if (Dice100.SuccessRoll(luckMod * 2))
             {
-                if (roll > 90)
+                if ((region == (int)DaggerfallRegions.OrsiniumArea || dungeon == (int)DFRegion.DungeonTypes.OrcStronghold) && Dice100.SuccessRoll(luckMod))
+                {
                     return WeaponMaterialTypes.Orcish;
-            }
-
-            if (matRoll > 98)
-            {
-                if (roll > 95)
-                    weaponMaterial = WeaponMaterialTypes.Daedric;
-                else if (roll > 80)
-                    weaponMaterial = WeaponMaterialTypes.Orcish;
+                }
                 else
-                    weaponMaterial = WeaponMaterialTypes.Ebony;
+                    matRoll += luckMod * 4;               
             }
-            else if (matRoll > 95)
+            if (matRoll > 90)
             {
-                weaponMaterial = WeaponMaterialTypes.Adamantium;
+                if (Dice100.SuccessRoll(luckMod * 3))
+                    weaponMaterial = RandomHighTier();
+                else
+                    weaponMaterial = WeaponMaterialTypes.Dwarven;
             }
-            else if (matRoll > 90)
-            {
-                weaponMaterial = WeaponMaterialTypes.Mithril;
-            }
-            else if (matRoll > 85)
-            {
-                weaponMaterial = WeaponMaterialTypes.Dwarven;
-            }
-            else if (matRoll > 75)
+            else if (matRoll > 83)
             {
                 weaponMaterial = WeaponMaterialTypes.Elven;
             }
-            else if (matRoll > 70)
+            else if (matRoll > 75)
             {
                 weaponMaterial = WeaponMaterialTypes.Silver;
             }
-            else if (matRoll > 30)
+            else if (matRoll > 35)
             {
                 weaponMaterial = WeaponMaterialTypes.Steel;
             }
+            return weaponMaterial;
+        }
+
+        private static WeaponMaterialTypes RandomHighTier()
+        {
+            WeaponMaterialTypes weaponMaterial = WeaponMaterialTypes.Mithril;
+
+            int roll = UnityEngine.Random.Range(0, 201);
+            if (roll > 199)
+                weaponMaterial = WeaponMaterialTypes.Daedric;
+            else if (roll > 192)
+                weaponMaterial = WeaponMaterialTypes.Orcish;
+            else if (roll > 180)
+                weaponMaterial = WeaponMaterialTypes.Ebony;
+            else if (roll > 100)
+                weaponMaterial = WeaponMaterialTypes.Adamantium;
+
 
             return weaponMaterial;
+        }
+
+        private static int dungeonQuality()
+        {
+            switch (dungeon)
+            {
+                case (int)DFRegion.DungeonTypes.VolcanicCaves:
+                case (int)DFRegion.DungeonTypes.Coven:
+                    return 21;
+                case (int)DFRegion.DungeonTypes.DesecratedTemple:
+                case (int)DFRegion.DungeonTypes.DragonsDen:
+                    return 18;
+                case (int)DFRegion.DungeonTypes.BarbarianStronghold:
+                case (int)DFRegion.DungeonTypes.VampireHaunt:
+                    return 15;
+                case (int)DFRegion.DungeonTypes.Crypt:
+                case (int)DFRegion.DungeonTypes.OrcStronghold:
+                    return 12;
+                case (int)DFRegion.DungeonTypes.Laboratory:
+                case (int)DFRegion.DungeonTypes.HarpyNest:
+                    return 10;
+                case (int)DFRegion.DungeonTypes.GiantStronghold:
+                    return 5;
+                case (int)DFRegion.DungeonTypes.HumanStronghold:
+                case (int)DFRegion.DungeonTypes.RuinedCastle:
+                case (int)DFRegion.DungeonTypes.Prison:
+                    return Mathf.Min(GameManager.Instance.PlayerEntity.Level, 15);
+            }
+
+            return 0;
         }
 
         private static WeaponMaterialTypes shopMat()
@@ -205,7 +262,7 @@ namespace UnleveledLootMod
             WeaponMaterialTypes weaponMaterial = WeaponMaterialTypes.Steel;
             int region = GameManager.Instance.PlayerGPS.CurrentRegionIndex;
             int shopQuality = GameManager.Instance.PlayerEnterExit.Interior.BuildingData.Quality;
-            matRoll = (matRoll - 40) + (shopQuality * 2);
+            matRoll = (matRoll - 35) + (shopQuality * 2);
 
             //increased chance of orcish when in orsinium
             if ((region == (int)DaggerfallRegions.OrsiniumArea) && (matRoll) > 70)
@@ -217,33 +274,25 @@ namespace UnleveledLootMod
 
             if (matRoll > 99)
             {
-                weaponMaterial = WeaponMaterialTypes.Daedric;
+                weaponMaterial = WeaponMaterialTypes.Ebony;
             }
             else if (matRoll > 97)
             {
-                weaponMaterial = WeaponMaterialTypes.Orcish;
-            }
-            else if (matRoll > 95)
-            {
-                weaponMaterial = WeaponMaterialTypes.Ebony;
-            }
-            else if (matRoll > 92)
-            {
                 weaponMaterial = WeaponMaterialTypes.Adamantium;
             }
-            else if (matRoll > 88)
+            else if (matRoll > 94)
             {
                 weaponMaterial = WeaponMaterialTypes.Mithril;
             }
-            else if (matRoll > 80)
+            else if (matRoll > 88)
             {
                 weaponMaterial = WeaponMaterialTypes.Dwarven;
             }
-            else if (matRoll > 70)
+            else if (matRoll > 80)
             {
                 weaponMaterial = WeaponMaterialTypes.Elven;
             }
-            else if (matRoll > 64)
+            else if (matRoll > 70)
             {
                 weaponMaterial = WeaponMaterialTypes.Silver;
             }
@@ -260,9 +309,9 @@ namespace UnleveledLootMod
             int roll = UnityEngine.Random.Range(0, 101) + (luckMod - 5);
             bool addDaedricItem = false;
             if ((enemyEntityID == (int)MobileTypes.DaedraLord && roll > 70)
-                || (enemyEntityID == (int)MobileTypes.DaedraSeducer && roll > 80)
-                || ((enemyEntityID == (int)MobileTypes.FireDaedra || enemyEntityID == (int)MobileTypes.FrostDaedra) && roll > 90)
-                || (enemyEntityID == (int)MobileTypes.Daedroth && roll > 95))
+                || (enemyEntityID == (int)MobileTypes.DaedraSeducer && roll > 75)
+                || ((enemyEntityID == (int)MobileTypes.FireDaedra || enemyEntityID == (int)MobileTypes.FrostDaedra) && roll > 80)
+                || (enemyEntityID == (int)MobileTypes.Daedroth && roll > 80))
             {
                 addDaedricItem = true;
             }
@@ -290,5 +339,39 @@ namespace UnleveledLootMod
             }
         }
 
+        private static void AddOrcish(ItemCollection loot, int enemyEntityID)
+        {
+            int roll = UnityEngine.Random.Range(0, 101) + (luckMod - 5);
+            bool addOrcishItem = false;
+            if ((enemyEntityID == (int)MobileTypes.OrcWarlord && roll > 80)
+                || (enemyEntityID == (int)MobileTypes.OrcShaman && roll > 90)
+                || (enemyEntityID == (int)MobileTypes.OrcSergeant && roll > 95)
+                || (enemyEntityID == (int)MobileTypes.Orc && roll > 98))
+            {
+                addOrcishItem = true;
+            }
+
+            if (addOrcishItem)
+            {
+                int typeRoll = UnityEngine.Random.Range(0, 100);
+                DaggerfallUnityItem item;
+                if (typeRoll > 50)
+                {
+                    Weapons weapon = (Weapons)UnityEngine.Random.Range((int)Weapons.Dagger, (int)Weapons.Long_Bow + 1);
+                    item = ItemBuilder.CreateItem(ItemGroups.Weapons, (int)weapon);
+                    ItemBuilder.ApplyWeaponMaterial(item, WeaponMaterialTypes.Orcish);
+                    item.currentCondition = (int)(UnityEngine.Random.Range(0.3f, 0.75f) * item.maxCondition);
+                    loot.AddItem(item);
+                }
+                else
+                {
+                    Armor armor = (Armor)UnityEngine.Random.Range((int)Armor.Cuirass, (int)Armor.Tower_Shield + 1);
+                    item = ItemBuilder.CreateItem(ItemGroups.Armor, (int)armor);
+                    ItemBuilder.ApplyArmorMaterial(item, ArmorMaterialTypes.Orcish);
+                    item.currentCondition = (int)(UnityEngine.Random.Range(0.3f, 0.75f) * item.maxCondition);
+                    loot.AddItem(item);
+                }
+            }
+        }
     }
 }
